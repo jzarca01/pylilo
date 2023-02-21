4b11e465b241402d5f9734a76a08692e7e9ef1c7
Jeremie Zarca <jeremie.zarca@gmail.com>
2023-02-21 18:30:56 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# OralB Toothbrush
+# LILO Pret a pousser
 [![PyPI Version](https://img.shields.io/pypi/v/OralB?label=PyPI&logo=pypi)](https://pypi.org/project/OralB/)
 
 
-Async method to read information from Bluetooth OralB Toothbrush.
+Async method to read information from Bluetooth LILO Pret a pousser.
 
 Exposes the following sensors:
- - Brush Time
- - Battery Life
- - Selected Mode
- - Operation status
- - Sector
- - Sector Time
+ - Time
+ - Light
--- pylilo/__init__.py
+++ pylilo/__init__.py
@@ -1,4 +1,4 @@
-"""Connect with OralB Toothbrush."""
+"""Connect with LILO."""
 
 import asyncio
 import logging
@@ -10,24 +10,20 @@ from bleak_retry_connector import BleakClient, BLEDevice, establish_connection
 _LOGGER = logging.getLogger(__name__)
 
 
-class OralB:
-    """Connects to OralB toothbrush to get information."""
+class LILO:
+    """Connects to LILO to get information."""
 
     def __init__(self, ble_device: BLEDevice) -> None:
         """Initialize the class object."""
         self.ble_device = ble_device
         self._cached_services = None
         self.client = None
-        self.name = "OralB"
+        self.name = "LILO"
         self.prev_time = 0
 
         self.result = {
-            "brush_time": None,
-            "battery": None,
-            "status": None,
-            "mode": None,
-            "sector": None,
-            "sector_time": None,
+            "time": None,
+            "light": None,
         }
 
     def set_ble_device(self, ble_device) -> None:
@@ -67,7 +63,7 @@ class OralB:
         self.client = None
 
     async def gatherdata(self):
-        """Connect to the OralB to get data."""
+        """Connect to the LILO to get data."""
         if self.ble_device is None:
             return self.result
         if time.time() - self.prev_time < 1:
@@ -75,36 +71,14 @@ class OralB:
         self.prev_time = time.time()
         await self.connect()
         chars = {
-            "a0f0ff08-5047-4d53-8208-4f72616c2d42": "time",
-            "a0f0ff05-5047-4d53-8208-4f72616c2d42": "battery",
-            "a0f0ff04-5047-4d53-8208-4f72616c2d42": "status",
-            "a0f0ff07-5047-4d53-8208-4f72616c2d42": "mode",
-            "a0f0ff09-5047-4d53-8208-4f72616c2d42": "sector",
+            "53e11633-b840-4b21-93ce-081726ddc739": "time",
+            "53e11632-b840-4b21-93ce-081726ddc739": "light",
         }
-        statuses = {
-            2: "IDLE",
-            3: "RUN",
-        }
-        modes = {
-            0: "OFF",
-            1: "DAILY_CLEAN",
-            7: "INTENSE",
-            2: "SENSITIVE",
-            4: "WHITEN",
-            3: "GUM_CARE",
-            6: "TONGUE_CLEAN",
-        }
-        sectors = {
-            0: "SECTOR_1",
-            1: "SECTOR_2",
-            2: "SECTOR_3",
-            3: "SECTOR_4",
-            4: "SECTOR_5",
-            5: "SECTOR_6",
-            7: "SECTOR_7",
-            8: "SECTOR_8",
-            "FE": "LAST_SECTOR",
-            "FF": "NO_SECTOR",
+        light = {
+            "00": "Off",
+            "01": "Photo mode",
+            "02": "Spring",
+            "03": "Summer" 
         }
         try:
             tasks = []
@@ -112,13 +86,10 @@ class OralB:
                 tasks.append(asyncio.create_task(self.client.read_gatt_char(char)))
             results = await asyncio.gather(*tasks)
             res_dict = dict(zip(chars.values(), results))
+            
+            self.result["time"] = res_dict["time"][0]
+            self.result["light"] = res_dict["light"][0]
 
-            self.result["brush_time"] = 60 * res_dict["time"][0] + res_dict["time"][1]
-            self.result["battery"] = res_dict["battery"][0]
-            self.result["status"] = statuses.get(res_dict["status"][0], "UNKNOWN")
-            self.result["mode"] = modes.get(res_dict["mode"][0], "UNKNOWN")
-            self.result["sector"] = sectors.get(res_dict["sector"][0], "UNKNOWN")
-            self.result["sector_time"] = res_dict["sector"][1]
         except Exception:
             _LOGGER.debug(f"{self.name}: Not connected to device")
 
@@ -126,10 +97,10 @@ class OralB:
 
 
 async def discover():
-    """Start looking for a OralB toothbrush."""
+    """Start looking for a LILO."""
     devices = await bleak.BleakScanner.discover()
     for device in devices:
-        if device.name == "Oral-B Toothbrush":
+        if device.name == "LILO":
             return device
 
 
@@ -137,10 +108,10 @@ async def main():
     """Run manually."""
     ble_device = await discover()
     _LOGGER.debug(ble_device)
-    orlb = OralB(ble_device)
+    lilo = LILO(ble_device)
     while True:
         time.sleep(1)
-        _LOGGER.debug(await orlb.gatherdata())
+        _LOGGER.debug(await lilo.gatherdata())
 
 
 if __name__ == "__main__":
